87b481b8f657d94cb997894372f1bd5861cdb979
Reibleister <106363632+Reibleister@users.noreply.github.com>
2026-02-11 23:49:45 +0100

    Script_Plataforma_Terminado

--- AceroHada_Project1/Assets/AceroHada_Root/Scripts/MovilSprite.cs
+++ AceroHada_Project1/Assets/AceroHada_Root/Scripts/MovilSprite.cs
@@ -1,6 +1,37 @@
+using System.Data;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MovilSprite : MonoBehaviour
 {
-   
+    [SerializeField] private Transform[] puntosMovimiento;
+    [SerializeField] private float velocidadMovimiento;
+    private int siguientePlataforma = 1;
+    private bool ordenPlataformas = true;
+
+    private void Update()
+    {
+        if(ordenPlataformas && siguientePlataforma + 1 >= puntosMovimiento.Length)
+        {
+            ordenPlataformas = false;
+        }
+        if (ordenPlataformas && siguientePlataforma <= 0)
+        {
+            ordenPlataformas = true;
+        }
+
+        if(Vector2.Distance(transform.position, puntosMovimiento[siguientePlataforma].position) < 0.1f) { 
+            if (ordenPlataformas)
+        {
+            siguientePlataforma += 1;
+        }
+        else
+        {
+            siguientePlataforma -= 1;
+        }
+    }
+    transform.position = Vector2.MoveTowards(transform.position, puntosMovimiento[siguientePlataforma].position,
+        velocidadMovimiento * Time.deltaTime);
+    }
 }
+
